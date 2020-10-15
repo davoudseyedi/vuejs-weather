3517d5f0fe735856d83b085fd0b18b01caf95537
dawid <davoudseyedi@gmail.com>
2020-10-15 19:25:48 +0330

    First commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import wb from "./registerServiceWorker";
 
-Vue.config.productionTip = false
+
+Vue.config.productionTip = false;
+Vue.prototype.$workbox = wb;
 
 new Vue({
   render: h => h(App),
